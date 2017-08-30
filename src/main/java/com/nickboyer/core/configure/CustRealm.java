9224c6bcd23ae8c5bc299192c8cdbd0506bbd297
nickboyer <nickboyer@foxmail.com>
2017-08-30 17:42:14 +0800

    add shiro/mybatis_generator

---
+++ src/main/java/com/nickboyer/core/configure/CustRealm.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 Buyforyou.cn All rights reserved
+ * 
+ * @author Kang.Y
+ * 
+ * @mail
+ * 
+ * @createtime 2017年8月30日 下午5:09:42
+ */
+package com.nickboyer.core.configure;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @title
+ * @description
+ * @since JDK1.8
+ */
+public class CustRealm extends AuthorizingRealm {
+
+	/*
+	 * （非 Javadoc）
+	 * 
+	 * @see org.apache.shiro.realm.AuthorizingRealm#doGetAuthorizationInfo(org.apache.shiro.subject.PrincipalCollection)
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+
+		return null;
+	}
+
+	/*
+	 * （非 Javadoc）
+	 * 
+	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+		return null;
+
+	}
+
+}
---
+++ src/main/java/com/nickboyer/core/util/RightsUtil.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2014 Buyforyou.cn All rights reserved
+ * 
+ * @author Kang.Y
+ * 
+ * @mail
+ * 
+ * @createtime 2017年8月30日 下午2:22:02
+ */
+package com.nickboyer.core.util;
+
+import java.math.BigInteger;
+
+/**
+ * 权限工具类
+ * 
+ * @title
+ * @description
+ * @since JDK1.8
+ */
+public class RightsUtil {
+
+	/**
+	 * 计算权限
+	 * 
+	 * @param rights
+	 * @return
+	 *
+	 * @authz Kang.Y
+	 * @createtime 2017年8月30日 下午2:25:17
+	 */
+	public static String sumRights(int[] rights) {
+
+		BigInteger num = new BigInteger("0");
+		for (int i : rights) {
+
+			num = num.setBit(i);
+		}
+
+		return num.toString();
+	}
+
+	/**
+	 * 计算权限
+	 * 
+	 * @param rights
+	 * @return
+	 *
+	 * @authz Kang.Y
+	 * @createtime 2017年8月30日 下午2:26:43
+	 */
+	public static String sumRights(String[] rights) {
+
+		BigInteger num = new BigInteger("0");
+		for (String i : rights) {
+
+			num = num.setBit(Integer.valueOf(i));
+		}
+		return num.toString();
+	}
+
+	/**
+	 * 测试权限
+	 * 
+	 * @param sum 权限和
+	 * @param rights 单个
+	 * @return
+	 *
+	 * @authz Kang.Y
+	 * @createtime 2017年8月30日 下午2:27:50
+	 */
+	public static Boolean testRights(BigInteger sum, int rights) {
+
+		try {
+			return sum.testBit(rights);
+
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * 测试权限
+	 * 
+	 * @param sum 权限和
+	 * @param rights 单个
+	 * @return
+	 *
+	 * @authz Kang.Y
+	 * @createtime 2017年8月30日 下午2:31:13
+	 */
+	public static Boolean testRights(String sum, String rights) {
+
+		try {
+
+			return new BigInteger(sum).testBit(Integer.valueOf(rights));
+		} catch (Exception e) {
+
+			return false;
+		}
+	}
+}
